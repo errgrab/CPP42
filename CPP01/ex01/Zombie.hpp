3fa0bc0f89e9d57c359505ca54414d2b317d94ab
ErikG <eriklustosa@gmail.com>
2024-12-21 18:14:13 +0000

    ex01 complete

---
+++ CPP01/ex01/Zombie.hpp
@@ -0,0 +1,19 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie {
+private:
+    std::string name;
+public:
+    Zombie();
+    ~Zombie();
+    void setName(std::string name);
+    void announce() const;
+};
+
+Zombie* zombieHorde(int N, std::string name);
+
+#endif
---
+++ CPP01/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "Zombie.hpp"
+#include <sstream>
+
+int main(int ac, char **av) {
+    int n = 5;
+    std::string name = "Zombie";
+    if (ac >= 2 && ac <= 3) {
+        std::stringstream ss(av[1]);
+        ss >> n;
+        if (ss.fail()) {
+            std::cerr << "Invalid input" << std::endl;
+            return 1;
+        }
+    }
+    if (ac == 3)
+        name = av[2];
+    Zombie *horde = zombieHorde(n, name);
+    for (int i = 0; i < n; i++) {
+        horde[i].announce();
+    }
+    delete[] horde;
+    return 0;
+}
