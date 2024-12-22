c1bb58f2627646b6347eb49ba510014fee2da7fa
ErikG <eriklustosa@gmail.com>
2024-12-22 02:00:03 +0000

    ex00 done!

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class Fixed {
+private:
+	int _value;
+	static const int _fractionalBits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed& fixed);
+	~Fixed();
+	Fixed& operator=(const Fixed& fixed);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
