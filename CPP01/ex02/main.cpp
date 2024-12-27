ca3c975c94eda3e645744df7672fe764d9f3fc87
Erik <eriklustosa@gmail.com>
2024-12-27 16:40:38 +0000

    fix ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,11 +6,11 @@ int main(void) {
     std::string &ref = var;
 
     std::cout << "Variable address: " << &var << std::endl;
-    std::cout << "Pointer address: " << &ptr << std::endl;
+    std::cout << "Pointer address: " << ptr << std::endl;
     std::cout << "Reference address: " << &ref << std::endl;
 
     std::cout << "Variable value: " << var << std::endl;
-    std::cout << "Pointer value: " << ptr << std::endl;
+    std::cout << "Pointer value: " << *ptr << std::endl;
     std::cout << "Reference value: " << ref << std::endl;
     return 0;
 }
