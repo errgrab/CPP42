315dfe412a82d0183fa2603b05e30f7e33ecd232
Erik <eriklustosa@gmail.com>
2024-12-27 16:44:56 +0000

    fix ex03

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -1,5 +1,4 @@
 #include <string>
-#include <iostream>
 #include "HumanA.hpp"
 #include "HumanB.hpp"
 #include "Weapon.hpp"
@@ -23,5 +22,5 @@ int main() {
         club.setType("some other type of club");
         jim.attack();
     }
-    return 0;   
+    return 0;
 }
