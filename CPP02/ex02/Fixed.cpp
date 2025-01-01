44d83232d1ee295c34a545706967977892900882
ErikG <eriklustosa@gmail.com>
2025-01-01 20:57:24 +0000

    ex02 Done!

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -2,30 +2,30 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() : _value(0) {
-	std::cout << "Default constructor called" << std::endl;
+//	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int fixed) {
-	std::cout << "Int constructor called" << std::endl;
+//	std::cout << "Int constructor called" << std::endl;
 	_value = fixed << _fractionalBits;
 }
 
 Fixed::Fixed(const float fixed) {
-	std::cout << "Float constructor called" << std::endl;
+//	std::cout << "Float constructor called" << std::endl;
 	_value = static_cast<int>(fixed * (1 << _fractionalBits) + (fixed >= 0 ? 0.5 : -0.5));
 }
 
 Fixed::Fixed(const Fixed& fixed) {
-	std::cout << "Copy constructor called" << std::endl;
+//	std::cout << "Copy constructor called" << std::endl;
 	*this = fixed;
 }
 
 Fixed::~Fixed() {
-	std::cout << "Destructor called" << std::endl;
+//	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(const Fixed& fixed) {
-	std::cout << "Copy assignment operator called" << std::endl;
+//	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &fixed)
 		_value = fixed.getRawBits();
 	return *this;
@@ -47,6 +47,85 @@ float Fixed::toFloat(void) const {
 
 int Fixed::toInt(void) const {
 	return _value >> _fractionalBits;
+}
+
+
+bool Fixed::operator>(const Fixed& fixed) const {
+	return _value > fixed.getRawBits();
+}
+
+bool Fixed::operator<(const Fixed& fixed) const {
+	return _value < fixed.getRawBits();
+}
+
+bool Fixed::operator>=(const Fixed& fixed) const {
+	return _value >= fixed.getRawBits();
+}
+
+bool Fixed::operator<=(const Fixed& fixed) const {
+	return _value <= fixed.getRawBits();
+}
+
+bool Fixed::operator==(const Fixed& fixed) const {
+	return _value == fixed.getRawBits();
+}
+
+bool Fixed::operator!=(const Fixed& fixed) const {
+	return _value != fixed.getRawBits();
+}
+
+Fixed Fixed::operator+(const Fixed& fixed) const {
+	return Fixed(toFloat() + fixed.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed& fixed) const {
+	return Fixed(toFloat() - fixed.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed& fixed) const {
+	return Fixed(toFloat() * fixed.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed& fixed) const {
+	return Fixed(toFloat() / fixed.toFloat());
+}
+
+Fixed& Fixed::operator++(void) {
+	_value++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int) {
+	Fixed tmp(*this);
+	operator++();
+	return tmp;
+}
+
+Fixed& Fixed::operator--(void) {
+	_value--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int) {
+	Fixed tmp(*this);
+	operator--();
+	return tmp;
+}
+
+Fixed& Fixed::min(Fixed& a, Fixed& b) {
+	return a < b ? a : b;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b) {
+	return a > b ? a : b;
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b) {
+	return a < b ? a : b;
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b) {
+	return a > b ? a : b;
 }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
--- CPP02/ex02/Fixed.hpp
+++ CPP02/ex02/Fixed.hpp
@@ -11,9 +11,34 @@ public:
 	Fixed(const float fixed);
 	Fixed(const Fixed& fixed);
 	~Fixed();
+
 	Fixed& operator=(const Fixed& fixed);
+
+	bool operator>(const Fixed& fixed) const;
+	bool operator<(const Fixed& fixed) const;
+	bool operator>=(const Fixed& fixed) const;	
+	bool operator<=(const Fixed& fixed) const;
+	bool operator==(const Fixed& fixed) const;
+	bool operator!=(const Fixed& fixed) const;
+
+	Fixed operator+(const Fixed& fixed) const;	
+	Fixed operator-(const Fixed& fixed) const;	
+	Fixed operator*(const Fixed& fixed) const;
+	Fixed operator/(const Fixed& fixed) const;
+
+	Fixed& operator++(void);
+	Fixed operator++(int);
+	Fixed& operator--(void);
+	Fixed operator--(int);
+
+	static Fixed& min(Fixed& a, Fixed& b);
+	static Fixed& max(Fixed& a, Fixed& b);
+	static const Fixed& min(const Fixed& a, const Fixed& b);
+	static const Fixed& max(const Fixed& a, const Fixed& b);
+
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	
 	float toFloat(void) const;
 	int toInt(void) const;
 };
