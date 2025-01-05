d080e054299d938575d8976b0eb54cd6cef1b0ab
Erik <Eriklustosa@gmail.com>
2025-01-05 02:56:31 +0000

    ???

---
+++ CPP04/ex02/Brain.cpp
@@ -0,0 +1,34 @@
+#include "Brain.hpp"
+
+Brain::Brain() {
+    std::cout << "Brain constructor called" << std::endl;
+    for (int i = 0; i < 100; i++) {
+        ideas[i] = "";
+    }
+}
+
+Brain::~Brain() {
+    std::cout << "Brain destructor called" << std::endl;
+}
+
+Brain::Brain(const Brain& brain) {
+    std::cout << "Brain copy constructor called" << std::endl;
+    for (int i = 0; i < 100; i++) {
+        ideas[i] = brain.getIdea(i);
+    }
+}
+
+Brain& Brain::operator=(const Brain& brain) {
+    for (int i = 0; i < 100; i++) {
+        ideas[i] = brain.getIdea(i);
+    }
+    return *this;
+}
+
+std::string Brain::getIdea(int index) const {
+    return ideas[index];
+}
+
+void Brain::setIdea(int index, std::string idea) {
+    ideas[index] = idea;
+}
