af9ddfba6e19d6af30cb990a914b8ae9c69103ea
Erik <eriklustosa@gmail.com>
2025-03-12 19:14:33 +0000

    CPP05/ex00 fist stage

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <exception>
+#include <iostream>
+
+class Bureaucrat {
+private:
+	const std::string name;
+	int grade;
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(const Bureaucrat &other);
+	~Bureaucrat();
+	Bureaucrat &opearator=(const Bureaucrat &other);
+
+	const std::string &getName() const;
+	int getGrade() const;
+
+	void incrementGrade();
+	void decrementGrade();
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
---
+++ CPP05/ex00/main.cpp
@@ -0,0 +1,7 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+int main(void) {
+	std::cout << "this is C++" << std::endl;
+	return (0);
+}
