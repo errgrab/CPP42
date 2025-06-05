8a3809851451a00a7b0273bb379d083869857bd6
ErikG <eriklustosa@gmail.com>
2025-06-05 19:17:14 +0100

    CPP05/ex00: Update.

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -9,33 +9,33 @@ void testBureaucrat() {
 		std::cout << high << std::endl;
 		high.incrementGrade();
 	} catch (std::exception &e) {
-		std::cout << "Exception caught: " << e.what() << std::endl;
+		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: grade decrement beyond valid range (grade 150)." << std::endl;
+	std::cout << "\nTest: grade decrement beyond valid range (grade 150)." << std::endl;
 	try {
 		Bureaucrat low("LowRank", 150);
 		std::cout << low << std::endl;
 		low.decrementGrade();
 	} catch (std::exception &e) {
-		std::cout << "Exception caught: " << e.what() << std::endl;
+		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: creating a Bureaucrat with grade 0." << std::endl;
+	std::cout << "\nTest: creating a Bureaucrat with grade 0." << std::endl;
 	try {
 		Bureaucrat invalid("AwA", 0);
 	} catch (std::exception &e) {
 		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: creating a Bureaucrat with grade 151." << std::endl;
+	std::cout << "\nTest: creating a Bureaucrat with grade 151." << std::endl;
 	try {
 		Bureaucrat invalid("UwU", 151);
 	} catch (std::exception &e) {
 		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: creating a Bureaucrat with grade 1." << std::endl;
+	std::cout << "\nTest: creating a Bureaucrat with grade 1." << std::endl;
 	try {
 		Bureaucrat valid("Valid", 1);
 		std::cout << valid << std::endl;
@@ -43,7 +43,7 @@ void testBureaucrat() {
 		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: creating a Bureaucrat with grade 150." << std::endl;
+	std::cout << "\nTest: creating a Bureaucrat with grade 150." << std::endl;
 	try {
 		Bureaucrat valid("ValidLow", 150);
 		std::cout << valid << std::endl;
@@ -51,7 +51,7 @@ void testBureaucrat() {
 		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: incrementing grade of a Bureaucrat with grade 75." << std::endl;
+	std::cout << "\nTest: incrementing grade of a Bureaucrat with grade 75." << std::endl;
 	try {
 		Bureaucrat mid("Mid", 75);
 		std::cout << mid << std::endl;
@@ -61,7 +61,7 @@ void testBureaucrat() {
 		std::cout << "Err: " << e.what() << std::endl;
 	}
 
-	std::cout << "Test: decrementing grade of a Bureaucrat with grade 75." << std::endl;
+	std::cout << "\nTest: decrementing grade of a Bureaucrat with grade 75." << std::endl;
 	try {
 		Bureaucrat mid("Mid", 75);
 		std::cout << mid << std::endl;
