4a7ca718815a24c35ffd5f02b15d094e0c9336c8
ErikG <eriklustosa@gmail.com>
2025-06-20 23:26:22 +0100

    CPP06/ex02: Done!

---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "Classes.hpp"
+
+Base *generate(void) {
+	std::srand(std::time(0));
+	int random = std::rand() % 3;
+	switch (random) {
+		case 0: return new A;
+		case 1: return new B;
+		case 2: return new C;
+	}
+	return NULL;
+}
+
+void identify(Base *p) {
+	if (dynamic_cast<A*>(p)) {
+		std::cout << "Class A" << std::endl;
+	} else if (dynamic_cast<B*>(p)) {
+		std::cout << "Class B" << std::endl;
+	} else if (dynamic_cast<C*>(p)) {
+		std::cout << "Class C" << std::endl;
+	} else {
+		std::cout << "Impossible!" << std::endl;
+	}
+}
+
+void identify(Base &p) {
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "Class A" << std::endl;
+		return;
+	} catch(...) {}
+	try {
+		(void)dynamic_cast<B&>(p);
+		std::cout << "Class B" << std::endl;
+		return;
+	} catch(...) {}
+	try {
+		(void)dynamic_cast<C&>(p);
+		std::cout << "Class C" << std::endl;
+		return;
+	} catch(...) {}
+	std::cout << "Impossible!" << std::endl;
+}
+
+int main(void) {
+	Base *randomClass = generate();
+	if (!randomClass) {
+		std::cout << "Failed to generate random class" << std::endl;
+		return 1;
+	}
+	identify(randomClass);
+	identify(*randomClass);
+	delete randomClass;
+	return 0;
+}
