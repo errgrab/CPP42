a94b01de279a85fcf742c3995b737b31f8dd6ee3
ErikG <eriklustosa@gmail.com>
2025-07-03 14:33:16 +0100

    CPP08/ex00: better tests

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -3,19 +3,46 @@
 #include <vector>
 #include <list>
 
-int main(void) {
+void testVector(void) {
+	std::cout << "=== Vector ===" << std::endl;
 	std::vector<int> intV;
 	intV.push_back(1);
 	intV.push_back(2);
 	intV.push_back(4);
 	std::vector<int>::iterator found;
-	for (int i = 0; i < 5; i++) {
+	std::cout << "It should find 1, 2 and 4." << std::endl;
+	for (int i = 0; i < 6; i++) {
+		std::cout << "Trying " << i << ": ";
 		try {
 			found = easyfind(intV, i);
-			std::cout << "Found: " << i << std::endl;
+			std::cout << "Value found." << std::endl;
 		} catch (std::exception &e) {
-			std::cout << e.what() << " Number: " << i << std::endl;
+			std::cout << e.what() << std::endl;
 		}
 	}
+}
+
+void testList(void) {
+	std::cout << "=== List ===" << std::endl;
+	std::list<int> intL;
+	intL.push_back(0);
+	intL.push_back(3);
+	intL.push_back(5);
+	std::list<int>::iterator found;
+	std::cout << "It should find 0, 3 and 5." << std::endl;
+	for (int i = 0; i < 6; i++) {
+		std::cout << "Trying " << i << ": ";
+		try {
+			found = easyfind(intL, i);
+			std::cout << "Value found." << std::endl;
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+}
+
+int main(void) {
+	testVector();
+	testList();
 	return 0;
 }
