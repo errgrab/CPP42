5b1d608b78adfbc593b124345c3491a9013839c8
Erik <eriklustosa@gmail.com>
2025-07-07 20:53:38 +0100

    CPP08/ex01: update on testing

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -18,6 +18,28 @@ int main(void) {
 	} catch (std::exception &e) {
 		std::cout << "Error: " << e.what() << std::endl;
 	}
+	std::cout << std::endl;
+
+	std::cout << "Try shortestSpan() without adding numbers, "
+		<< "should print error." << std::endl;
+	try {
+		sp = Span(5);
+		std::cout << sp.shortestSpan() << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "Try shortestSpan() with only one number, "
+		<< "should print error." << std::endl;
+	try {
+		sp = Span(5);
+		sp.addNumber(0);
+		std::cout << sp.shortestSpan() << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
 
 	return 0;
 }
