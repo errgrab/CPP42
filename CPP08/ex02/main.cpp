02b4e1b4dc61a187b95970ffff7052e7acf1e57d
ErikG <eriklustosa@gmail.com>
2025-07-10 05:51:00 +0100

    CPP08/ex02: Update tests

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -4,27 +4,27 @@
 
 int main()
 {
-	std::list<int> mstack;
+	MutantStack<int> mstack;
 
-	mstack.push_back(5);
-	mstack.push_back(17);
+	mstack.push(5);
+	mstack.push(17);
 
-	std::cout << mstack.back() << std::endl;
+	std::cout << mstack.top() << std::endl;
 
-	mstack.pop_back();
+	mstack.pop();
 
 	std::cout << mstack.size() << std::endl;
 
-	mstack.push_back(3);
-	mstack.push_back(5);
-	mstack.push_back(737);
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
 
 	//[...]
 
-	mstack.push_back(0);
+	mstack.push(0);
 
-	std::list<int>::iterator it = mstack.begin();
-	std::list<int>::iterator ite = mstack.end();
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
 
 	++it;
 	--it;
@@ -35,7 +35,7 @@ int main()
 		++it;
 	}
 
-	std::list<int> s(mstack);
+	MutantStack<int> s(mstack);
 
 	return 0;
 }
